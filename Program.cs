26d60af501338eee4be3bfcbf28606d91ee1196c
Kairatbek23 <82087081+Kairatbek23@users.noreply.github.com>
2024-04-22 12:34:01 -0700

    HELLO WORLD CHANGED
    BLA BLA

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Homework 
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Add your code here. Starting with greeting the user
+            Console.WriteLine("Hello, Csd228!");
+        }
+    }
+}
